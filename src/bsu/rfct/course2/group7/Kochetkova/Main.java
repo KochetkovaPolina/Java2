9489593bc5755a254ee9feae72920bae2710386d
KochetkovaPolina <kochetkova1303@gmail.com>
2023-12-19 22:40:32 +0300

    sdfsdf

---
+++ src/bsu/rfct/course2/group7/Kochetkova/Main.java
@@ -0,0 +1,13 @@
+package bsu.rfct.course2.group7.Kochetkova;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        calc.setVisible(true);
+
+
+    }
+}
